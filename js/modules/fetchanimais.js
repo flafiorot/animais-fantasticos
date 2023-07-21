62ee2bd8c6df9a2c05d033ade9d7d076ae25f5c4
Flávia Costa <fla.fiorot@gmail.com>
2023-07-21 11:00:08 -0300

    fetch animais refatorado

--- js/modules/fetchanimais.js
+++ js/modules/fetchanimais.js
@@ -1,30 +1,45 @@
 import AnimaNumeros from './anima-numeros.js';
 
-export default function initFetchAnimais() {
+export default function fetchAnimais(url, target) {
+  // Cria a div contendo informações com 
+  // o total de animais
   function createAnimal(animal) {
     const div = document.createElement('div');
     div.classList.add('numero-animal');
     div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.total}`;
-
     return div;
   }
 
-  async function fetchAnimais(url) {
+  // Preenche cada animal no DOM
+  const numerosGrid = document.querySelector(target);
+
+  function preencherAnimais(animal) {
+    const divAnimal = createAnimal(animal);
+    numerosGrid.appendChild(divAnimal);
+  }
+
+  // Anima os números de cada animal
+  function animaAnimaisNumeros() {
+    const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
+    animaNumeros.init();
+  }
+
+  // Puxa os animais através de arquivo json
+  // e cria cada animal utilizando createAnimal
+  async function criarAnimais() {
     try {
+      // fetch, espera resposta e transforma resposta em json
       const animaisResponse = await fetch(url);
       const animaisJSON = await animaisResponse.json();
-      const numerosGrid = document.querySelector('.numeros-grid');
 
-      animaisJSON.forEach((animal) => {
-        const divAnimal = createAnimal(animal);
-        numerosGrid.appendChild(divAnimal);
-      });
-      const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
-      animaNumeros.init();
+      // Após transformar json, ativa as funções
+      // para preencher e animar os numeros
+      animaisJSON.forEach(animal => preencherAnimais(animal));
+      animaAnimaisNumeros();
     } catch (erro) {
       console.log(Error(erro));
     }
   }
 
-  fetchAnimais('./animaisapi.json');
+  return criarAnimais();
 }
